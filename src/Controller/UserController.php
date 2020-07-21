dba8f53bbd4f3017f398a1d220c891397865e257
PlamenPanayotov <p.panayotov@mail.bg>
2020-07-21 22:50:00 +0300

    edit profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\EditUserType;
 use App\Service\User\UserServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class UserController extends AbstractController
 {
@@ -15,7 +20,7 @@ class UserController extends AbstractController
         $this->userService = $userService;
     }
     /**
-     * @Route("/profile", name="user_profile")
+     * @Route("/user/profile", name="user_profile")
      */
     public function profile()
     {
@@ -28,5 +33,40 @@ class UserController extends AbstractController
             'user' => $user,
             'isVerified' => $isVerified
         ]);
+    }
+
+    /**
+     * @Route("/user/edit", name="user_edit")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     */
+    public function editUser(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = $this->userService->currentUser();
+        $isVerified = $user->isVerified();
+        
+        $form = $this->createForm(EditUserType::class, $user);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $user->setRoles(['ROLE_USER']);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_profile');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'editUserForm' => $form->createView(),
+        ]);
+        
     }
 }
